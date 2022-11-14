c6ab05898b17a0051b4df258bad8ddf21033aa23
Patch Neranartkomol <patch@neranartkomol.com>
2022-11-13 22:03:42 -0800

    Valid Parentheses

---
+++ stack/valid_parentheses.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+OPEN_PARENTHESES = {'(', '{', '['}
+
+
+class Solution:
+    """
+    https://leetcode.com/problems/valid-parentheses
+
+    Iterate through the input string, adding open parens to stack.
+
+    Compare closing parens in string to top element of stack.
+
+    Time Complexity: O(N)
+    Space Complexity: O(N)
+    """
+
+    def is_valid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c in OPEN_PARENTHESES:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                check = stack.pop()
+                if check == '(' and c != ')':
+                    return False
+                elif check == '{' and c != '}':
+                    return False
+                elif check == '[' and c != ']':
+                    return False
+
+        return True if not stack else False
+
+
+class TestSolution(TestCase):
+    solution = Solution()
+
+    def test_basic_valid_input(self):
+        s = "()"
+        self.assertTrue(self.solution.is_valid(s))
+
+    def test_extended_valid_input(self):
+        s = "()[]{}"
+        self.assertTrue(self.solution.is_valid(s))
+
+    def test_invalid(self):
+        s = "(]"
+        self.assertFalse(self.solution.is_valid(s))
